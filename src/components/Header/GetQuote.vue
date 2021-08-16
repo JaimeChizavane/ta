dd91452488e6ec37c4a6df96086e48c3fac64a2c
Maizer Gomes <maizer.gomes@gmail.com>
2021-08-16 10:19:59 +0200

    Enable language changes

--- src/components/Header/GetQuote.vue
+++ src/components/Header/GetQuote.vue
@@ -2,11 +2,11 @@
   <li>
     <ul class="navbar-actions list-unstyled mb-0 d-flex align-items-center">
       <li class="d-none d-xl-block">
-        <a href="request-quote.html" class="btn action__btn-contact">Submeta a sua consulta</a>
+        <a href="#" class="btn action__btn-contact">Submeta a sua consulta</a>
       </li>
       <li>
         <button class="action__btn action__btn-login open-login-popup">
-          <i class="icon-user"></i><span>Entrar</span>
+          <i class="icon-user"></i><span>{{ $tc('Entrar') }}</span>
         </button>
       </li>
     </ul><!-- /.navbar-actions -->
--- src/components/Header/SelectLanguage.vue
+++ src/components/Header/SelectLanguage.vue
@@ -1,19 +1,61 @@
 <template>
   <li>
     <div class="dropdown lang-dropdown">
-      <button class="dropdown-toggle lang-dropdown-toggle" id="langDropdown" data-toggle="dropdown">
-        <i class="icon-map"></i><span>PT</span>
+      <button class="lang-dropdown-toggle" id="langDropdown" @click.prevent="changeLanguage">
+        <i class="icon-map"></i><span>{{ activeLanguage.name }}</span>
       </button>
-      <div class="dropdown-menu" aria-labelledby="langDropdown">
-        <a class="dropdown-item" href="#">EN</a>
-      </div>
     </div>
   </li>
 </template>
 
 <script>
 export default {
-  name: "QSelectLanguage"
+  name: "QSelectLanguage",
+  mounted () {
+    let active = window.localStorage.getItem('language')
+
+    if (active) {
+      this.activeLanguage = JSON.parse(active)
+    } else {
+      this.activeLanguage = this.languages[0]
+    }
+  },
+  methods: {
+    changeLanguage () {
+      if (this.activeLanguage.order === this.languages.length - 1) {
+        this.activeLanguage = this.languages[0]
+      } else {
+        this.activeLanguage = this.languages[this.activeLanguage.order + 1]
+      }
+
+      this.$i18n.locale = this.activeLanguage.locale
+
+      this.$eventHub.$emit('language_changes', this.activeLanguage.locale)
+
+      window.localStorage.setItem('language', JSON.stringify(this.activeLanguage))
+    }
+  },
+  data() {
+    return {
+      activeLanguage: {},
+      languages: [
+        {
+          flag: 'mz',
+          name: 'PT',
+          full_name: 'Português',
+          locale: 'pt',
+          order: 0
+        },
+        {
+          flag: 'za',
+          name: 'EN',
+          full_name: 'English',
+          locale: 'en',
+          order: 1
+        }
+      ]
+    }
+  }
 }
 </script>
 
