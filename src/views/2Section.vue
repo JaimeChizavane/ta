cfa8545fd40c7bebaab83f12176a951f6ce54349
jaimechizavane <jaime.jcm@gmail.com>
2021-09-13 14:18:33 +0200

    Changes in sections

--- src/views/2Section.vue
+++ src/views/2Section.vue
@@ -7,25 +7,51 @@
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-7 col-xl-6">
             <div class="heading heading-layout2 mb-50">
-              <h5> De acordo com o artigo n.º 1 do Decreto n.º 58/2010 de 14 de
-                Dezembro, os serviços de apoio do Tribunal Administrativo compreendem as seguintes unidades orgânicas:
-              </h5>
-              <ul class="list-items list-items-layout2 list-unstyled">
-                <li> Contadoria da Conta Geral do Estado;</li>
-                <li> Contadoria de Contas e Auditorias;</li>
-                <li> Contadoria do Visto;</li>
-                <li> Direcção de Administração e Finanças;</li>
-                <li> Direcção de Recursos Humanos;</li>
-                <li> Direcção de Sistema de Informação e Comunicação;</li>
-                <li> Direcção de Planificação e Cooperação;</li>
-                <li> Gabinete de Estudos;</li>
-                <li> Gabinete Jurídico;</li>
-                <li> Gabinete do Presidente;</li>
-                <li> Cartório da 1.ª Secção;</li>
-                <li> Cartório da 2.ª Secção;</li>
-                <li> Departamento de Documentação, Biblioteca e Arquivo;</li>
-                <li> Secretária-Geral.</li>
-              </ul>
+              <h3>CONTENCIOSO FISCAL E ADUANEIRO</h3>
+              <h4>2.ª SECÇÃO</h4>
+              <br>
+              <h5>O que é o Contencioso Fiscal e Aduaneiro?</h5>
+              <p>
+                O Contencioso Fiscal e Aduaneiro, também designado por Contencioso Tributário é um Contencioso judicial e representa a via necessária para assegurar os direitos dos contribuintes.
+ 
+O acesso à justiça é o único meio para afastar as ilegalidades praticadas pelas autoridades administrativas ou para reconhecer os direitos dos particulares.
+ 
+A 2.ª Secção do Tribunal Administrativo (TA), designada por Secção do Contencioso Fiscal e Aduaneiro dirime litígios, em segunda instância, isto é, nos casos em que os reclamantes ou recorrentes não concordam com as decisões da primeira instância, quer dizer dos tribunais fiscais ou dos tribunais aduaneiros e dos actos que dizem respeito a questões fiscais ou aduaneiras de quaisquer autoridades, não atribuídas, por lei, a outras instâncias.
+              </p>
+              <br>
+              <h5>Secção do Contencioso Fiscal e Aduaneiro</h5>
+              <p>
+                A 2.ͣ Secção - Contencioso Fiscal e Aduaneiro é uma das Secções do Tribunal Administrativo. De acordo com o n.ͦ 1 do artigo 228 da Constituição da República, o Tribunal Administrativo é o órgão superior da hierarquia dos tribunais administrativos, fiscais e aduaneiros de 1.ͣInstância.
+ 
+Conforme estabelece o artigo 30 da Lei n.ͦ 2 24/2013, de 1 de Novembro, republicada pela Lei n.ͦ 7/2015, de 6 de Outubro, concernente ao melhoramento do controlo da legalidade dos actos administrativos, bem como a fiscalização da legalidade das receitas e despesas públicas, compete à 2.ª Secção, conhecer:
+              </p>
+              <br>
+              <p>
+                 Os recursos dos actos respeitantes a questões fiscais e aduaneiras de quaisquer autoridades, excepto os emanados pelo Conselho de Ministros ou seu titular e do Primeiro-Ministro;
+·         Os pedidos relativos à execução dos seus acórdãos, mesmo que tenha sido interposto recurso para o Plenário;
+·         Os pedidos de produção antecipada da prova;
+·         A  suspensão  da  eficácia  dos  actos  referidos  no  n.ͦ 1,  quando  seja prestada a devida garantia;
+·         Os recursos das decisões dos tribunais fiscais e dos tribunais aduaneiros de primeira instância;
+·         Outras matérias atribuídas par lei.
+·         Estão excluídos da jurisdição fiscal e aduaneira
+·         Os actos relativos à instrução criminal e ao exercício da acção penal que não constituam infracções jurídico-fiscais ou infracções aduaneiras;
+·         Os bens e actos qualificados como pertencendo ao domínio público, excepto os casos de confisco, perda e abandono previstos na legislação aduaneira;
+·         Qualquer sujeito das relações jurídico-fiscais ou aduaneiras, que considere que os seus direitos e interesses legalmente protegidos pela lei tributária, tenham sido violados;
+·         A Administração Tributária sempre que pretenda garantir seus créditos tributários.
+              </p>
+              <br>
+             <p>
+<b>Prazo para interposição dos recursos e seus efeitos</b>
+             <br>
+ Das decisões dos tribunais fiscais, cabe recurso para a 2.ª Secção do Tribunal Administrativo, no prazo de 8 (oito) dias, devendo juntar as respectivas alegações, conforme dispõe o artigo 18.ͦ do Regulamento do Contencioso das Contribuições e Impostos, aprovado pelo Diploma Legislativo n.ͦ 783, de 18 de Abril de 1942.
+As  decisões  dos  tribunais  aduaneiros  são  recorríveis  para  a  2.ª Secção do Tribunal Administrativo, no prazo de 10 (dez) dias e, da admissão do recurso tem os arguidos ou recorrentes, 15 (quinze) dias para juntar as respectivas alegações, de acordo com o estabelecido no artigo  185.ͦ do  Contencioso  Aduaneiro,  aprovado  pelo  Decreto  n.º 33:531, de 21 de Fevereiro de 1944.
+·         Os recursos podem ter efeito devolutivo ou suspensivo. 0 Primeiro consiste em devolver o conhecimento do pleito do tribunal para quem recorre e o segundo em suspender a execução da decisão de que se recorre, até a sua resolução.
+·         Os recursos fiscais têm efeito meramente devolutivo, excepto se os contribuintes ou transgressores depositarem as importâncias impugnadas dos impostos ou das multas em que tiverem sido condenados, ou garantia bancária adequada e prevista na lei.
+·         Os recursos aduaneiros têm efeito suspensivo.
+ <br>
+              </p>
+              <br>
+
             </div>
           </div><!-- /.col-xl-5 -->
           <div class="col-sm-12 col-md-12 col-lg-5 col-xl-5 offset-xl-1">
