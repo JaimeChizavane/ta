b69f61bede54c601d20bcb92d13914726a59fbbf
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-03-01 10:28:10 +0200

    Update Resolution.vue

--- src/views/Resolution.vue
+++ src/views/Resolution.vue
@@ -123,7 +123,7 @@ import QHeader from "@/components/Header/Header";
 import QBreadCrumb from "@/components/BreadCrumb";
 
 export default {
-  name: "QLaw",
+  name: "QResolution",
   components: { QBreadCrumb, QHeader, QFooter },
   methods: {
     clear() {
@@ -179,8 +179,8 @@ export default {
     window.mainExecution()
 
     this.$http.get("legislacaoAll.json").then((data) => {
-     // this.allItems = data.data.d.results.filter(item => (item?.Tipo === 'Resolução' || item?.Tipo_x0020_de_x0020_Legisla_x00e === 'Resolução'))
-       this.allItems = data.data.d.results.filter(item => (item.Tipo === 'Resolução' || item.Tipo_x0020_de_x0020_Legisla_x00e === 'Resolução'))
+      this.allItems = data.data.d.results.filter(item => (item?.Tipo === 'Resolução' || item?.Tipo_x0020_de_x0020_Legisla_x00e === 'Resolução'))
+     //  this.allItems = data.data.d.results.filter(item => (item.Tipo === 'Resolução' || item.Tipo_x0020_de_x0020_Legisla_x00e === 'Resolução'))
       this.items = this.allItems
       // this.searcheable = this.items.flatMap((item) => {
       //   if (item.AttachmentFiles.results) {
