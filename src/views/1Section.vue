9e321ed23fd45836f227a06db9cde191f7cd615f
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-03-08 09:34:42 +0200

    automatic image change on institution

--- src/views/1Section.vue
+++ src/views/1Section.vue
@@ -5,6 +5,14 @@
     <section id="overview" class="features-layout3 pt-130 pb-130">
       <div class="container">
         <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12 col-xl-5">
+            <div class="about__img mb-40">  
+                <img :src="getImageUrl(services)" alt="blog image" class="cover__image"> 
+              <blockquote class="blockquote mb-0">
+                <h4 class="blockquote__title">{{services.Title }}</h4>
+              </blockquote>
+            </div><!-- /.about-img -->
+          </div><!-- /.col-xl-5 -->
           <div class="col-sm-12 col-md-12 col-lg-7 col-xl-6">
             <div class="heading heading-layout2 mb-50" v-html="services.Content"></div>
           </div><!-- /.col-xl-5 -->
@@ -28,6 +36,11 @@ import QBreadCrumb from "@/components/BreadCrumb";
 export default {
   name: "QSectionOne",
   components: { QBreadCrumb, QHeader, QFooter },
+  methods: {
+    getImageUrl(item) {
+      return item && item.Attachments ? process.env.VUE_APP_ROOT_DOCS + item.AttachmentFiles.results[0].ServerRelativeUrl : 'assets/images/about/1.jpg'
+    },
+  },
   data() {
     return {
       services: {
--- src/views/2Section.vue
+++ src/views/2Section.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="wrapper">
-    <q-header/>
-    <q-bread-crumb/>
+    <q-header />
+    <q-bread-crumb />
     <section id="overview" class="features-layout3 pt-130 pb-130">
       <div class="container">
         <div class="row">
+          <div class="col-sm-12 col-md-12 col-lg-12 col-xl-5">
+            <div class="about__img mb-40">
+              <img
+                :src="getImageUrl(services)"
+                alt="blog image"
+                class="cover__image"
+              />
+              <blockquote class="blockquote mb-0">
+                <h4 class="blockquote__title">{{ services.Title }}</h4>
+              </blockquote>
+            </div>
+            <!-- /.about-img -->
+          </div>
+          <!-- /.col-xl-5 -->
           <div class="col-sm-12 col-md-12 col-lg-7 col-xl-6">
-            <div class="heading heading-layout2 mb-50" v-html="services.Content"></div>
-          </div><!-- /.col-xl-5 -->
+            <div
+              class="heading heading-layout2 mb-50"
+              v-html="services.Content"
+            ></div>
+          </div>
+          <!-- /.col-xl-5 -->
           <div class="col-sm-12 col-md-12 col-lg-5 col-xl-5 offset-xl-1">
             <div class="sticky-top">
-              <img src="assets/images/sliders/8.png" alt="banner">
+              <img src="assets/images/sliders/8.png" alt="banner" />
             </div>
-          </div><!-- /col-lg-5 -->
-        </div><!-- /.row -->
-      </div><!-- /.container -->
-    </section><!-- /.Features Layout 3 -->
-    <q-footer/>
+          </div>
+          <!-- /col-lg-5 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /.Features Layout 3 -->
+    <q-footer />
   </div>
 </template>
 
@@ -28,24 +50,34 @@ import QBreadCrumb from "@/components/BreadCrumb";
 export default {
   name: "QSectionTwo",
   components: { QBreadCrumb, QHeader, QFooter },
+  methods: {
+    getImageUrl(item) {
+      return item && item.Attachments
+        ? process.env.VUE_APP_ROOT_DOCS +
+            item.AttachmentFiles.results[0].ServerRelativeUrl
+        : "assets/images/about/1.jpg";
+    },
+  },
   data() {
     return {
       services: {
-        Content: null
-      }
-    }
+        Content: null,
+      },
+    };
   },
   mounted() {
-    window.mainExecution()
-    this.$http.get("instituicao.json").then((data) => {
-      this.services = data.data.d.results.find(item => item.Id === 5)
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
-}
+    window.mainExecution();
+    this.$http
+      .get("instituicao.json")
+      .then((data) => {
+        this.services = data.data.d.results.find((item) => item.Id === 5);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
