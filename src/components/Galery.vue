73ca2684ad9cdefc6ac35fb49453024fc4bafd9e
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-03-08 10:24:23 +0200

    galery

--- src/components/Galery.vue
+++ src/components/Galery.vue
@@ -5,6 +5,32 @@
       <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-6 col-xl-7">
           <div class="banners-wrapper sticky-top">
+            <div class="slick-carousel carousel-arrows-light carousel-dots-light m-slides-0" data-slick='{"slidesToShow": 1, "arrows": true, "dots": true, "speed": 700,"fade": true,"cssEase": "linear", "autoplay": true}'>
+              <div class="slide-item align-v-h bg-overlay bg-overlay-gradient"  v-for="(image, index) in allImages" :key="index">
+                <div class="bg-img"><img :src="image.src" alt="slide img"></div>
+                <div class="container">
+                  <div class="row align-items-center">
+                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-7">
+                      <div class="slide__content">
+        <!--                <span class="slide__subtitle" v-html="item.subtitle"></span>-->
+        <!--                <h2 class="slide__title">{{ item.title }}</h2>-->
+                        <h1 class="pagetitle__heading">{{ image.title }}</h1>
+                        <p class="slide__desc" v-html="image.Name"></p>
+        <!--                <div class="d-flex flex-wrap align-items-center">-->
+        <!--                  <router-link :to="item.to" class="btn btn__primary btn__primary-style2 mr-30">-->
+        <!--                    <i class="icon-arrow-right"></i>-->
+        <!--                    <span> Saber Mais</span>-->
+        <!--                  </router-link>-->
+        <!--                </div>-->
+                      </div><!-- /.slide-content -->
+                    </div><!-- /.col-xl-7 -->
+
+
+                  </div><!-- /.row -->
+                </div><!-- /.container -->
+              </div><!-- /.slide-item -->
+           </div><!-- /.carousel -->
+            
             <div class="tab-content mb-50">
               <div class="tab-pane fade" :class="[activeIndex === index ? 'show active' : '']"
                    v-for="(image, index) in allImages"
