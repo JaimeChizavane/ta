0a311d10b034867e9e6e77dfa1535d4820f735d6
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-03-14 09:46:09 +0200

    Update Sections

--- src/views/1Section.vue
+++ src/views/1Section.vue
@@ -2,28 +2,31 @@
   <div class="wrapper">
     <q-header/>
     <q-bread-crumb/>
-    <section id="overview" class="features-layout3 pt-130 pb-130">
+    <section class="about-layout2 pt-130 pb-90">
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-12 col-xl-5">
-            <div class="about__img mb-40">  
-                <img :src="getImageUrl(services)" alt="blog image" class="cover__image"> 
+            <div class="about__img mb-40 sticky-top">  
+                <img :src="getImageUrl(history)" alt="blog image" class="cover__image"> 
               <blockquote class="blockquote mb-0">
-                <h4 class="blockquote__title">{{services.Title }}</h4>
+                <h4 class="blockquote__title">{{ history.Title }}</h4>
               </blockquote>
             </div><!-- /.about-img -->
           </div><!-- /.col-xl-5 -->
-          <div class="col-sm-12 col-md-12 col-lg-7 col-xl-6">
-            <div class="heading heading-layout2 mb-50" v-html="services.Content"></div>
-          </div><!-- /.col-xl-5 -->
-         <!--  <div class="col-sm-12 col-md-12 col-lg-5 col-xl-5 offset-xl-1">
-            <div class="sticky-top">
-              <img src="assets/images/sliders/8.png" alt="banner">
+          <div class="col-sm-12 col-md-12 col-lg-12 col-xl-6 offset-xl-1">
+            <!-- <div class="heading-layout2 mb-40">
+              <h3 class="heading__title">{{ history.Title }}</h3>
+            </div> /heading --> 
+            <div class="about-text-wrapper">
+              <div class="about__Text">
+                <p class="font-weight-bold mb-30" v-html="history.Content"></p>
+              </div>
+
             </div>
-          </div> /col-lg-5 --> 
+          </div><!-- /.col-xl-7 -->
         </div><!-- /.row -->
       </div><!-- /.container -->
-    </section><!-- /.Features Layout 3 -->
+    </section><!-- /.About Layout 2 -->
     <q-footer/>
   </div>
 </template>
@@ -43,15 +46,18 @@ export default {
   },
   data() {
     return {
-      services: {
-        Content: null
+      history: {
+        Content: null,
+        Attachments: null
       }
     }
   },
   mounted() {
     window.mainExecution()
+
     this.$http.get("instituicao.json").then((data) => {
-      this.services = data.data.d.results.find(item => item.Id === 4)
+      this.history = data.data.d.results.find(item => item.Id === 4);
+      console.log(history);
     }).catch((error) => {
       console.log(error)
     })
--- src/views/2Section.vue
+++ src/views/2Section.vue
@@ -1,44 +1,33 @@
 <template>
   <div class="wrapper">
-    <q-header />
-    <q-bread-crumb />
-    <section id="overview" class="features-layout3 pt-130 pb-130">
+    <q-header/>
+    <q-bread-crumb/>
+    <section class="about-layout2 pt-130 pb-90">
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-12 col-xl-5">
-            <div class="about__img mb-40 sticky-top">
-              <img
-                :src="getImageUrl(services)"
-                alt="blog image"
-                class="cover__image"
-              />
+            <div class="about__img mb-40 sticky-top">  
+                <img :src="getImageUrl(history)" alt="blog image" class="cover__image"> 
               <blockquote class="blockquote mb-0">
-                <h4 class="blockquote__title">{{ services.Title }}</h4>
+                <h4 class="blockquote__title">{{ history.Title }}</h4>
               </blockquote>
+            </div><!-- /.about-img -->
+          </div><!-- /.col-xl-5 -->
+          <div class="col-sm-12 col-md-12 col-lg-12 col-xl-6 offset-xl-1">
+            <!-- <div class="heading-layout2 mb-40">
+              <h3 class="heading__title">{{ history.Title }}</h3>
+            </div> /heading --> 
+            <div class="about-text-wrapper">
+              <div class="about__Text">
+                <p class="font-weight-bold mb-30" v-html="history.Content"></p>
+              </div>
+
             </div>
-            <!-- /.about-img -->
-          </div>
-          <!-- /.col-xl-5 -->
-          <div class="col-sm-12 col-md-12 col-lg-7 col-xl-6">
-            <div
-              class="heading heading-layout2 mb-50"
-              v-html="services.Content"
-            ></div>
-          </div>
-          <!-- /.col-xl-5 -->
-        <!--   <div class="col-sm-12 col-md-12 col-lg-5 col-xl-5 offset-xl-1">
-            <div class="sticky-top">
-              <img src="assets/images/sliders/8.png" alt="banner" />
-            </div>
-          </div> -->
-          <!-- /col-lg-5 -->
-        </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container -->
-    </section>
-    <!-- /.Features Layout 3 -->
-    <q-footer />
+          </div><!-- /.col-xl-7 -->
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </section><!-- /.About Layout 2 -->
+    <q-footer/>
   </div>
 </template>
 
@@ -52,32 +41,30 @@ export default {
   components: { QBreadCrumb, QHeader, QFooter },
   methods: {
     getImageUrl(item) {
-      return item && item.Attachments
-        ? process.env.VUE_APP_ROOT_DOCS +
-            item.AttachmentFiles.results[0].ServerRelativeUrl
-        : "assets/images/about/1.jpg";
+      return item && item.Attachments ? process.env.VUE_APP_ROOT_DOCS + item.AttachmentFiles.results[0].ServerRelativeUrl : 'assets/images/about/1.jpg'
     },
   },
   data() {
     return {
-      services: {
+      history: {
         Content: null,
-      },
-    };
+        Attachments: null
+      }
+    }
   },
   mounted() {
-    window.mainExecution();
-    this.$http
-      .get("instituicao.json")
-      .then((data) => {
-        this.services = data.data.d.results.find((item) => item.Id === 5);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-};
+    window.mainExecution()
+
+    this.$http.get("instituicao.json").then((data) => {
+      this.history = data.data.d.results.find(item => item.Id === 5);
+      console.log(history);
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+}
 </script>
 
 <style scoped>
+
 </style>
