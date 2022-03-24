e03fa049f7ee0ba9819a15f14cd68a6671ba1d8d
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-03-24 13:18:00 +0200

    Update GetQuote.vue

--- src/components/Header/GetQuote.vue
+++ src/components/Header/GetQuote.vue
@@ -1,13 +1,14 @@
 <template>
   <li>
     <ul class="navbar-actions list-unstyled mb-0 d-flex align-items-center">
-      <li class="d-none d-xl-block">
+      <!-- <li class="d-none d-xl-block">
         <a href="#" class="btn action__btn-contact">Submeta a sua consulta</a>
-      </li>
-      <li>
-        <button class="action__btn action__btn-login open-login-popup">
+      </li> -->
+      <li class="d-none d-xl-block">
+        <a href="https://mail.ta.gov.mz/" class="btn" target="_blank"><i class="fa-solid fa-envelope-circle-check" ></i><span class="">Webmail</span></a>
+        <!-- <button class="action__btn action__btn-login open-login-popup">
           <i class="icon-user"></i><span>{{ $tc('Entrar') }}</span>
-        </button>
+        </button> -->
       </li>
     </ul><!-- /.navbar-actions -->
   </li>
