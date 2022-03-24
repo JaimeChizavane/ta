6a2bc9c24b8652e9bc9b6171b86afc4942a99d18
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-03-24 13:28:24 +0200

    webmail href

--- src/components/Header/GetQuote.vue
+++ src/components/Header/GetQuote.vue
@@ -5,7 +5,7 @@
         <a href="#" class="btn action__btn-contact">Submeta a sua consulta</a>
       </li> -->
       <li class="d-none d-xl-block">
-        <a href="https://mail.ta.gov.mz/" class="action__btn" target="_blank"><i class="fa-solid fa-envelope-circle-check" ></i><span class="">Webmail</span></a>
+        <a href="https://mail.ta.gov.mz/" class="action__btn" target="_blank"><i class="fa-solid fa-envelope-circle-check" ></i> <img src="assets/images/logo/webmail.png" class="logo-light" alt="logo"></a>
         <!-- <button class="action__btn action__btn-login open-login-popup">
           <i class="icon-user"></i><span>{{ $tc('Entrar') }}</span>
         </button> -->
