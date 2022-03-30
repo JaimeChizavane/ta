157fcfdac040f513be1d0be92cf409b2b1f2b8ff
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-03-30 15:12:08 +0200

    GOOGLE ANALYTICS API START

--- src/components/Header/GetQuote.vue
+++ src/components/Header/GetQuote.vue
@@ -1,9 +1,18 @@
 <template>
   <li>
     <ul class="navbar-actions list-unstyled mb-0 d-flex align-items-center">
-      <!-- <li class="d-none d-xl-block">
-        <a href="#" class="btn action__btn-contact">Submeta a sua consulta</a>
-      </li> -->
+      <li class="d-none d-xl-block">
+        <span class="">
+        Online
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+            2
+           </span>
+        </span>
+ 
+      </li>
+         <li class="d-none d-xl-block">
+       
+      </li>
       <li class="d-none d-xl-block">
         <a href="https://mail.ta.gov.mz/" class="action__btn" target="_blank"><i class="fa-solid fa-envelope-circle-check" ></i> <img src="assets/images/logo/webmail.png" class="logo-light" alt="logo"></a>
         <!-- <button class="action__btn action__btn-login open-login-popup">
