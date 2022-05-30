804c407e159869ec05f7ce78827ec8ac02343bb9
Maizer Gomes <maizer.gomes@gmail.com>
2022-05-30 17:26:41 +0200

    Fix cors

--- src/components/Header/GetQuote.vue
+++ src/components/Header/GetQuote.vue
@@ -34,10 +34,10 @@ window.io = require('socket.io-client')
 
 const echoinstance = process.env.VUE_APP_WEBSOCKET_URL ? new Echo({
   broadcaster: 'socket.io',
-  host: window.location.hostname + ':8081',
-  transports: ['websocket', 'polling', 'flashsocket'] // Fix CORS error!
+  // host: window.location.hostname + ':8081',
+  transports: ['websocket', 'polling', 'flashsocket'], // Fix CORS error!
   // broadcaster: 'socket.io',
-  // host: process.env.VUE_APP_WEBSOCKET_URL,
+  host: process.env.VUE_APP_WEBSOCKET_URL,
   // path: process.env.VUE_APP_WEBSOCKET_PATH,
   // namespace: 'App.Events'
 }) : {}
