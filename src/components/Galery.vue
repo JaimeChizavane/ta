b507248ce28de06f1b6c95a9223cd56bcb21c579
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-06-20 12:27:24 +0200

    Update Galery.vue

--- src/components/Galery.vue
+++ src/components/Galery.vue
@@ -1,86 +1,122 @@
 <template>
   <section class="work-process pt-130 pb-130">
-    <div class="bg-img"><img src="assets/images/backgrounds/2.jpg" alt="background"></div>
+    <div class="bg-img">
+      <img src="assets/images/backgrounds/2.jpg" alt="background" />
+    </div>
     <div class="container">
-     
-       <div class="row">  
-        <div class="col-sm-12 col-md-12 col-lg-6 col-xl-7">
+      <div class="row justify-content-sm-center">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
           <div class="banners-wrapper sticky-top">
-            <div class="tab-content mb-50">
-              <div class="tab-pane fade" :class="[activeIndex === index ? 'show active' : '']"
-                   v-for="(image, index) in allImages"
-                   :key="index">
-                <div class="video__banner">
-                  <img :src="image.src" class="rounded" alt="banner">
-                </div><!-- /.video__banner -->
+            <section class="slider" v-if="sliders.length">
+              <div
+                class="
+                  slick-carousel
+                  carousel-arrows-light carousel-dots-light
+                  m-slides-0
+                "
+                data-slick='{"slidesToShow": 1, "arrows": true, "dots": true, "speed": 700,"fade": true,"cssEase": "linear", "autoplay": true}'
+              >
+                <div
+                  class="slide-item"
+                  v-for="(item, index) in sliders"
+                  :key="index"
+                >
+                  <div class="bg-img text-center">
+                    <img :src="item.img" alt="slide img" />
+                  </div>
+                  <div class="container">
+                    <div class="row align-items-center">
+                      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                        <div class="slide__content text-center mt-0 text-block__title"> 
+                            <span
+                              class="text-light "
+                              v-html="item.subtitle"
+                            ></span> 
+
+                          <!--             <h1 class="process-item__title">{{ item.title }}</h1>    <div class="d-flex flex-wrap align-items-center">-->
+                          <!--                  <router-link :to="item.to" class="btn btn__primary btn__primary-style2 mr-30">-->
+                          <!--                    <i class="icon-arrow-right"></i>-->
+                          <!--                    <span> Saber Mais</span>-->
+                          <!--                  </router-link>-->
+                          <!--                </div>-->
+                        </div>
+                        <!-- /.slide-content -->
+                      </div>
+                      <!-- /.col-xl-7 -->
+                    </div>
+                    <!-- /.row -->
+                  </div>
+
+                  <!-- /.container -->
+                </div>
+
+                <!-- /.slide-item -->
               </div>
-            </div><!-- /.tab-content -->
-            <div class="cta-banner mt-30 mb-30 d-flex flex-wrap align-items-center">
+              <!-- /.carousel -->
+            </section>
+            <div
+              class="cta-banner mt-30 mb-30 d-flex flex-wrap align-items-center"
+            >
               <h4 class="cta__title my-3 pr-30">
                 Encontre a nossa galeria de imagens online.
               </h4>
-              <router-link :to="{name: 'galery'}" class="btn btn__primary btn__primary-style2">
+              <router-link
+                :to="{ name: 'galery' }"
+                class="btn btn__primary btn__primary-style2"
+              >
                 <span> Ver mais </span>
                 <i class="icon-arrow-right"></i>
               </router-link>
-            </div> <!-- /.cta-banner -->
+            </div>
           </div>
-        </div><!-- /.col-lg-7 -->
-        <div class="col-sm-12 col-md-12 col-lg-6 col-xl-5">
-          <div class="heading mb-70">
-            <h2 class="heading__subtitle color-primary">Galeria</h2>
-           <!-- <h3 class="heading__title color-white">Encontre a nossa galeria digital! </h3>-->
-          </div>
-          <nav class="nav nav-tabs">
-            <!-- process Item #1 -->
-            <a class="process-item col-12" :class="[activeIndex == index ? 'active' : '']" @click.prevent="activeIndex = index" href="#"
-               v-for="(image, index) in allImages"
-               :key="index">
-              <div class="process-item__content">
-                <h4 class="process-item__title">{{ image.Title }}</h4>
-               <!-- <p class="process-item__desc">{{ image.Name }}</p>-->
-              </div>
-            </a><!-- /.process-item -->
-          </nav>
-        </div><!-- /.col-lg-5 -->
-      </div><!-- /.row -->
-    </div><!-- /.container -->
-  </section><!-- /.Work Process -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.slider -->
 </template>
 
 <script>
 export default {
-  name: "QGalery",
-  methods: {
-    getFileUrl(item) {
-      return item && item.ServerRelativeUrl ? process.env.VUE_APP_ROOT_DOCS + item.ServerRelativeUrl : '#'
-    }
+  name: "QSlider",
+  watch: {
+    news: function () {
+      // this.sliders = this.sliders.concat(newVal)
+      // window.$('.slick-carousel').slick();
+    },
   },
   data() {
     return {
-      allImages: [],
-      activeIndex: 0
-    }
+      sliders: [
+        {
+          img: "assets/images/about/1.jpg",
+          subtitle:
+            "Seja bem-vindo ao site do Tribunal Administrativo de Moçambique.",
+          title: "O Tribunal Administrativo",
+          desc: "É o órgão superior da hierarquia dos tribunais administrativos.",
+          to: { name: "history" },
+        },
+        {
+          img: "assets/images/sliders/2.jpg",
+          subtitle: "Atribuições e Competências do Tribunal Administrativo",
+          title: "Tribunal Administrativo em prol da Justiça",
+          desc: "O Tribunal Administrativo é o órgão superior da hierarquia dos tribunais administrativos provinciais e da Cidade de Maputo, dos tribunais fiscais e dos tribunais aduaneiros.",
+          to: { name: "history" },
+        },
+      ],
+    };
   },
-  mounted() {
-    // window.mainExecution()
-    this.$http.get("images.json").then((data) => {
-      this.allImages = data.data.d.results.filter(f => f.Name !== 'Forms').flatMap(f => f.Folders.results
-          .flatMap(fo => fo.Files.results
-              .map((file) => {
-                file.Title = file.Title && file.Title.trim() ? file.Title : fo.Name
-                file.src = this.getFileUrl(file)
-
-                return file
-              }))).slice(0, 5)
-
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
-}
+  methods: {
+    getImageUrl(item) {
+      return item && item.Attachments
+        ? process.env.VUE_APP_ROOT_DOCS +
+            item.AttachmentFiles.results[0].ServerRelativeUrl
+        : "assets/images/blog/grid/1.jpg";
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>
