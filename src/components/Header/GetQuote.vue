64024d42c1fcda22a647277e29e1cc9be6dd94b6
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2022-08-22 11:03:02 +0200

    chenge color from #8C1717 to #6e1a3e, and logo

--- src/components/Header/GetQuote.vue
+++ src/components/Header/GetQuote.vue
@@ -3,89 +3,107 @@
     <ul class="navbar-actions list-unstyled mb-0 d-flex align-items-center">
       <li class="d-none d-xl-block">
         <span>
-        Online: 
-          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary"
-                id="count_visitors">
+          Online:
+          <span
+            class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary"
+            id="count_visitors"
+          >
             {{ usersCount }}
-           </span>
-           </span>
-           <span>
-        Visitas : 
-          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary"
-                id="count_visitors">
-            {{ usersCount }}
-           </span>
-           </span>
-
-
+          </span>
+        </span>
       </li>
+      <li class="d-none d-xl-block"></li>
       <li class="d-none d-xl-block">
-
+        <span>
+          Visitas :
+          <span
+            class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary"
+            id="count_visitors"
+          >
+            {{ usersCount }}
+          </span>
+        </span>
       </li>
-      <li class="d-none d-xl-">
-        <a href="https://mail.ta.gov.mz/" class="action__btn" target="_blank"><i
-            class="fa-solid fa-envelope-circle-check"></i> <img src="assets/images/logo/webmail.png" class="logo-light"
-                                                                alt="logo"></a>
+      <li class="d-none d-xl-block"></li>
+      <li class="d-none d-xl-block">
+        <a href="https://mail.ta.gov.mz/" class="action__btn" target="_blank"
+          ><i class="fa-solid fa-envelope-circle-check"></i>
+          <img
+            src="assets/images/logo/webmail.png"
+            class="logo-light"
+            alt="logo"
+        /></a>
+
         <!-- <button class="action__btn action__btn-login open-login-popup">
           <i class="icon-user"></i><span>{{ $tc('Entrar') }}</span>
         </button> -->
       </li>
-    </ul><!-- /.navbar-actions -->
+       <li class="d-none d-xl-block"></li>
+       <li class="d-none d-xl-block"></li>
+       <li class="d-none d-xl-block"></li>
+       <li class="d-none d-xl-block"></li>
+       <li class="d-none d-xl-block"></li>
+    </ul>
+    <!-- /.navbar-actions -->
   </li>
 </template>
 
 <script>
-import Echo from 'laravel-echo'
+import Echo from "laravel-echo";
 
-window.io = require('socket.io-client')
-window.Pusher = require('pusher-js')
+window.io = require("socket.io-client");
+window.Pusher = require("pusher-js");
 
-const echoinstance = process.env.VUE_APP_WEBSOCKET_HOST ? new Echo({
-  key: process.env.VUE_APP_WEBSOCKET_KEY,
-  wsHost: process.env.VUE_APP_WEBSOCKET_HOST,
-  // wsPort: process.env.VUE_APP_WEBSOCKET_PORT,
-  // wssPort: process.env.VUE_APP_WEBSOCKET_PORT,
-  authEndpoint: process.env.VUE_APP_WEBSOCKET_AUTH,
-  broadcaster: 'pusher',
-  // key: "a08cd5563a35608a631a71bf7d66ea68",
-  //     ws://localhost:6001/app/
-  // wsHost: "localhost",
-  wsPort: 8082,
-  wssPort: 8082,
-  forceTLS: false,
-  encrypted: true,
-  disableStats: true,
-  enabledTransports: ['ws', 'wss'],
-  // authEndpoint: "https://api.ta.test/broadcasting/auth"
-}) : {}
+const echoinstance = process.env.VUE_APP_WEBSOCKET_HOST
+  ? new Echo({
+      key: process.env.VUE_APP_WEBSOCKET_KEY,
+      wsHost: process.env.VUE_APP_WEBSOCKET_HOST,
+      // wsPort: process.env.VUE_APP_WEBSOCKET_PORT,
+      // wssPort: process.env.VUE_APP_WEBSOCKET_PORT,
+      authEndpoint: process.env.VUE_APP_WEBSOCKET_AUTH,
+      broadcaster: "pusher",
+      // key: "a08cd5563a35608a631a71bf7d66ea68",
+      //     ws://localhost:6001/app/
+      // wsHost: "localhost",
+      wsPort: 8082,
+      wssPort: 8082,
+      forceTLS: false,
+      encrypted: true,
+      disableStats: true,
+      enabledTransports: ["ws", "wss"],
+      // authEndpoint: "https://api.ta.test/broadcasting/auth"
+    })
+  : {};
 
 export default {
   name: "QGetQuote",
   data() {
     return {
       users: [],
-      usersCount: 0
-    }
+      usersCount: 0,
+    };
   },
   mounted() {
-    echoinstance.connect()
+    echoinstance.connect();
 
-    echoinstance.join('chat').here((users) => {
-          this.users = users;
-          this.usersCount = users.length;
+    echoinstance
+      .join("chat")
+      .here((users) => {
+        this.users = users;
+        this.usersCount = users.length;
 
-          console.log(users)
-        })
-        .joining((user) => {
-          console.log(user)
-          this.users.push(user);
-          this.usersCount = this.usersCount + 1;
-        })
-        .leaving((user) => {
-          console.log(user)
-          // this.users.$remove(user);
-          this.usersCount = this.usersCount - 1;
-        });
+        console.log(users);
+      })
+      .joining((user) => {
+        console.log(user);
+        this.users.push(user);
+        this.usersCount = this.usersCount + 1;
+      })
+      .leaving((user) => {
+        console.log(user);
+        // this.users.$remove(user);
+        this.usersCount = this.usersCount - 1;
+      });
 
     // echoinstance.channel('test')
     //   .listen('event', (e) => {
@@ -108,11 +126,9 @@ export default {
   },
   methods: {},
   beforeDestroy() {
-    echoinstance.disconnect()
-  }
-}
+    echoinstance.disconnect();
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,28 +2,44 @@
   <header class="header header-transparent header-full">
     <nav class="navbar navbar-expand-lg sticky-navbar">
       <div class="container">
-        <router-link class="navbar-brand" :to="{name : 'home'}">
-          <img src="assets/images/logo/logo.png" class="logo-light" alt="logo">
+        <router-link class="navbar-brand" :to="{ name: 'home' }" style="margin-right:15px">
+          <img
+            src="assets/images/logo/logo.png"
+            class="logo-light"
+            alt="logo"
+            style="max-height: 100px;"
+          />
           <!-- <img src="assets/images/logo/logo-dark.png" class="logo-dark" alt="logo"> -->
         </router-link>
         <button class="navbar-toggler" type="button" @click="openMenu()">
           <span class="menu-lines"><span></span></span>
         </button>
         <div class="header__top-right">
-          <ul class="list-unstyled d-flex justify-content-end align-items-center mb-0">
-<!--            <q-select-language/>-->
-            <q-get-quote/>
+          <ul
+            class="list-unstyled d-flex justify-content-end align-items-center mb-0"
+          >
+            <!--            <q-select-language/>-->
+            <q-get-quote />
           </ul>
-        </div><!-- /.header-top-right -->
-        <q-menu/>
-        <q-search/>
-        <!-- <router-link class="navbar-brand ml-40" :to="{name : 'home'}">
-          <img src="assets/images/logo/Logotipo.jpg" class="logo-light" alt="logo">
-        
-        </router-link> -->
-      </div><!-- /.container -->
-    </nav><!-- /.navabr -->
-  </header><!-- /.Header -->
+        </div>
+        <!-- /.header-top-right -->
+        <q-menu />
+        <q-search />
+        <li class="d-none d-xl-block"></li>
+        <router-link class="navbar-brand" :to="{ name: 'home' }" style="margin-left:15px">
+          <img
+            src="assets/images/logo/Logotipo.jpg"
+            class="logo-light"
+            alt="logo"
+            style="max-height: 100px;"
+          />
+        </router-link>
+      </div>
+      <!-- /.container -->
+    </nav>
+    <!-- /.navabr -->
+  </header>
+  <!-- /.Header -->
 </template>
 
 <script>
@@ -35,15 +51,13 @@ import QGetQuote from "@/components/Header/GetQuote";
 export default {
   name: "QHeader",
   // components: { QSelectLanguage, QGetQuote, QSearch, QMenu },
-  components: {QGetQuote, QSearch, QMenu },
+  components: { QGetQuote, QSearch, QMenu },
   methods: {
     openMenu() {
       document.getElementById("mainNavigation").classList.add("menu-opened");
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
