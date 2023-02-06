b719cdde14eeef44a1db0bfe73d4f3cc1c1fb394
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2023-02-06 15:16:23 +0200

    Update FetchConcursosCommand.php

--- api/app/Console/Commands/FetchConcursosCommand.php
+++ api/app/Console/Commands/FetchConcursosCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Criado por Maizer Aly de O. Gomes para api.
  * Email: maizer.gomes@gmail.com / maizer.gomes@ekutivasolutions / maizer.gomes@outlook.com
@@ -17,6 +18,8 @@ class FetchConcursosCommand extends SPApiFetch
      * @var \App\Services\HttpClient
      */
     protected $client;
-    protected $uri  = "_api/web/lists/getByTitle('Concursos')/items?\$expand=Folder/Files";
+    // protected $uri  = "_api/web/lists/getByTitle('Concursos')/items?\$expand=Folder/Files";
+
+    protected $uri  = "_api/web/GetFolderByServerRelativeUrl('/Concursos')/Folders?\$expand=Files&\$orderBy=Created desc";
     protected $path = 'concursos.json';
 }
