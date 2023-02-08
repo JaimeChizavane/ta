7a19e6ad2830ae3cd2b76266c4a28dccac426e0d
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2023-02-08 11:52:03 +0200

    Update Contests.vue

--- src/views/Contests.vue
+++ src/views/Contests.vue
@@ -1,19 +1,10 @@
+@@ -0,0 +1,104 @@
 <template>
   <div class="wrapper">
     <q-header />
     <q-bread-crumb />
     <section class="careers">
       <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-12 col-lg-6 offset-lg-3">
-            <div class="heading text-center mb-50">
-              <h3 class="heading__title">{{ $tc($route.meta.display) }}</h3>
-            </div>
-            <!-- /.heading -->
-          </div>
-          <!-- /.col-lg-10 -->
-        </div>
-        <!-- /.row -->
         <div class="row">
           <div class="col-sm-12 col-md-12 col-lg-12 col-xl-6 offset-xl-3">
             <div class="pagetitle__form mb-50">
@@ -28,94 +19,62 @@
           </div>
           <!-- /.col-xl-6 -->
         </div>
-
-        <div class="container">
-          <div class="row" id="accordion">
-            <div class="col-sm-12 col-md-12 col-lg-12">
-              <div
-                class="accordion-item"
-                v-for="(faq, index) in history"
-                :key="index"
-              >
-                <div
-                  class="accordion__header"
-                  data-toggle="collapse"
-                  :data-target="'#collapse' + index"
-                >
-                  <a class="accordion__title" @click.prevent>{{
-                    faq.Folder.Name
-                  }}</a>
-                </div>
-                <!-- /.accordion-item-header -->
-                <div
-                  :id="'collapse' + index"
-                  class="collapse"
-                  data-parent="#accordion"
-                >
-                  <div class="accordion__body">
-                    <div class="row">
-                      <div class="col-12">
-                        <div class="jobs-container">
-                          <!-- career item #1 -->
-                          <div
-                            class="job-item"
-                            v-for="(item, index) in faq.Folder.Files.results"
-                            :key="index"
-                          >
-                            <div class="row">
-                              <div class="col-sm-12 col-md-12 col-lg-4">
-                                <div class="job__meta">
-                                  <span
-                                    class="job__type"
-                                    v-show="item.TimeLastModified"
-                                  >
-                                    {{ item.TimeLastModified | date }}
-                                  </span>
-                                </div>
-                                <!--   <h4 class="job__title">{{ item.Name || 'Sem titulo' }}</h4> -->
-                              </div>
-                              <!-- /.col-lg-4 -->
-                              <div class="col-sm-12 col-md-12 col-lg-5">
-                                <h4 class="job__title" v-html="item.Name"></h4>
-                              </div>
-                              <!-- /.col-lg-5 -->
-                              <div
-                                class="col-sm-12 col-md-12 col-lg-3 d-flex align-items-center justify-content-end btn-wrap"
-                              >
-                                <a
-                                  :href="getFileUrl(item)"
-                                  target="_blank"
-                                  class="btn btn__secondary"
-                                  >Abrir</a
-                                >
-                              </div>
-                              <!-- /.col-lg-3 -->
-                            </div>
-                            <!-- /.row -->
-                          </div>
-                          <!-- /.job-item -->
-                        </div>
-                      </div>
-                      <!-- /.col-lg-12 -->
+        <div class="row">
+          <div class="col-12">
+            <div class="jobs-container">
+              <!-- career item #1 -->
+              <div class="job-item" v-for="(item, index) in items" :key="index">
+                <div class="row">
+                  <div class="col-sm-12 col-md-12 col-lg-4">
+                    <div class="job__meta">
+                      <span class="job__type">{{ item.Tipo }}</span>
                     </div>
-                    <!-- /.row -->
+                    <h4 class="job__title">{{ item.Tipo }}</h4>
                   </div>
-                  <!-- /.accordion-item-body -->
+                  <!-- /.col-lg-4 -->
+                  <div class="clo-sm-12 col-md-12 col-lg-8">
+                    <div
+                      class="row"
+                      v-for="(file, index) in item.Folder.Files.results"
+                      :key="index + 'File'"
+                    >
+                      <div class="col-sm-12 col-md-12 col-lg-9">
+                        <p class="job__desc" v-html="file.Name"></p>
+                      </div>
+                      <!-- /.col-lg-5 -->
+                      <div
+                        class="col-sm-12 col-md-12 col-lg-3 d-flex align-items-center justify-content-end btn-wrap mb-2"
+                      >
+                        <a
+                          :href="getFileUrl(file)"
+                          target="_blank"
+                          class="btn btn__secondary"
+                          >Abrir</a
+                        >
+                      </div>
+                      <!-- /.col-lg-3 -->
+                    </div>
+                  </div>
+                  <!--                  <div class="col-sm-12 col-md-12 col-lg-8" v-if="item.Folder">-->
+                  <!--                    <a target="_blank" :href="getFileUrl(file)" v-for="(file, index) in item.Folder.Files.results"-->
+                  <!--                       :key="index">-->
+                  <!--                      <p class="job__desc mb-20">{{ file.Name }}</p>-->
+                  <!--                    </a>-->
+                  <!--                  </div>&lt;!&ndash; /.col-lg-5 &ndash;&gt;-->
                 </div>
+                <!-- /.row -->
               </div>
+              <!-- /.job-item -->
             </div>
-            <!-- /.col-lg-6 -->
           </div>
-          <!-- /.row -->
+          <!-- /.col-lg-12 -->
         </div>
-        <!-- /.container -->
+        <!-- /.row -->
       </div>
       <!-- /.container -->
     </section>
     <!-- /.careers -->
     <q-footer />
-    <CoolLightBox :items="slideImages" :index="index" @close="index = null">
-    </CoolLightBox>
   </div>
 </template>
 
@@ -124,62 +83,41 @@ import QFooter from '@/components/Footer';
 import QHeader from '@/components/Header/Header';
 import QBreadCrumb from '@/components/BreadCrumb';
 
-import CoolLightBox from 'vue-cool-lightbox';
-import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css';
-
 export default {
-  name: 'QStrategicPlan',
-  components: { QBreadCrumb, QHeader, QFooter, CoolLightBox },
+  name: 'QAboutUs',
+  components: { QBreadCrumb, QHeader, QFooter },
   methods: {
-    search() {
-      if (this.query) {
-        this.history = this.allItems.filter(
-          (item) =>
-            item.Name?.toLowerCase().includes(this.query.toLowerCase()) ||
-            item.Files.results.find((f) =>
-              f.Name?.toLowerCase().includes(this.query.toLowerCase())
-            )
-        );
-      } else {
-        this.history = this.allItems;
-      }
-    },
     getFileUrl(item) {
       return item && item.ServerRelativeUrl
         ? process.env.VUE_APP_ROOT_DOCS + item.ServerRelativeUrl
         : '#';
     },
-    getFileThumb(item) {
-      return item && item.ServerRelativeUrl
-        ? process.env.VUE_APP_ROOT_DOCS +
-            '/thumbs/' +
-            item.ServerRelativeUrl +
-            '.png'
-        : '#';
-    },
-    zoomImage(file) {
-      this.slideImages = [
-        {
-          src: this.getFileThumb(file),
-          description: file.Name,
-        },
-      ];
-      this.index = 0;
-    },
-    filterImages(name = null) {
-      this.activeFilter = name;
-
-      if (name) {
-        this.items = this.allItems;
+    search() {
+      if (this.query) {
+        this.items = this.allItems
+          .filter(
+            (item) =>
+              item.Tipo && item.Folder.Files && item.Folder.Files.results.length
+          )
+          .filter(
+            (item) =>
+              item.Folder.Name.toLowerCase().includes(
+                this.query.toLowerCase()
+              ) ||
+              item.Tipo.toLowerCase().includes(this.query.toLowerCase()) ||
+              item.Folder.Files.results.find((i) =>
+                i.Name.toLowerCase().includes(this.query.toLowerCase())
+              )
+          );
       } else {
-        this.items = this.allItems;
+        this.items = this.allItems.filter((item) => item.Folder.Files);
       }
     },
   },
   data() {
     return {
+      items: [],
       allItems: [],
-      history: [],
       query: '',
     };
   },
@@ -189,10 +127,8 @@ export default {
     this.$http
       .get('concursos.json')
       .then((data) => {
-        this.history = data.data.d.results.filter((i) => i.Name !== 'Forms');
-
-        this.allItems = this.history;
-        console.log(this.allItems);
+        this.allItems = data.data.d.results;
+        this.items = this.allItems.filter((item) => item.Folder.Files);
       })
       .catch((error) => {
         console.log(error);
