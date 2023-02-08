53dc23e10f76b4eac17d34afcf9df4a54d1a280c
Candido Ernesto Barato <50318137+Candido-Barato@users.noreply.github.com>
2023-02-08 16:26:19 +0200

    Update FetchOportunitiesCommand.php

--- api/app/Console/Commands/FetchOportunitiesCommand.php
+++ api/app/Console/Commands/FetchOportunitiesCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Criado por Maizer Aly de O. Gomes para api.
  * Email: maizer.gomes@gmail.com / maizer.gomes@ekutivasolutions / maizer.gomes@outlook.com
@@ -17,7 +18,7 @@ class FetchOportunitiesCommand extends SPApiFetch
      * @var \App\Services\HttpClient
      */
     protected $client;
-//    protected $uri  = "_api/web/GetFolderByServerRelativeUrl('/Oportunidades%20de%20Emprego')/Files";
-    protected $uri  = "_api/web/lists/getByTitle('Oportunidades%20de%20Emprego')/items?\$expand=File";
+    //    protected $uri  = "_api/web/GetFolderByServerRelativeUrl('/Oportunidades%20de%20Emprego')/Files";
+    protected $uri  = "_api/web/lists/getByTitle('Oportunidades%20de%20Emprego')/items?\$expand=Folder/Files";
     protected $path = 'oportunities.json';
 }
