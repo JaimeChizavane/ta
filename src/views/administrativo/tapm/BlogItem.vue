8de250e0a8aa42916edbc05738523f53fb12cf8a
Edson Chambal <echambal@ta.gov.mz>
2023-04-04 12:39:28 +0200

    Criacao dos T da Jurisdicao
    Criacao do TA da Provincia de Maputo

---
+++ src/views/administrativo/tapm/BlogItem.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="wrapper">
+    <q-header />
+    <q-bread-crumb />
+    <sub-menu />
+    <q-item
+      :item="item"
+      :navigation="navigation"
+      :related="related"
+      route-name="tadpn.blog-item"
+    />
+    <q-footer />
+  </div>
+</template>
+
+<script>
+import QFooter from '@/components/Footer';
+import QHeader from '@/components/Header/Header';
+import QBreadCrumb from '@/components/BreadCrumb';
+import QItem from '@/components/Item';
+import SubMenu from '@/views/aduaneiro/tadpn/components/SubMenu';
+
+export default {
+  name: 'QBlogItem',
+  components: { SubMenu, QItem, QBreadCrumb, QHeader, QFooter },
+  data() {
+    return {
+      news: [],
+    };
+  },
+  watch: {
+    item: function(val) {
+      this.$route.meta.display = val.Title;
+    },
+  },
+  computed: {
+    item: function() {
+      return (
+        this.news.find((article) => {
+          return article.GUID === this.$route.params.guid;
+        }) || {}
+      );
+    },
+    related: function() {
+      let items =
+        this.news.filter((article) => {
+          return ![
+            this.item.GUID,
+            this.navigation.next.GUID,
+            this.navigation.previous.GUID,
+          ].includes(article.GUID);
+        }) || [];
+
+      return items
+        .sort((item, next) => {
+          return (
+            new Date(next.Data_x0020_Noticia || next.Created) -
+            new Date(item.Data_x0020_Noticia || item.Created)
+          );
+        })
+        .slice(0, 4);
+    },
+    navigation: function() {
+      let prev = this.news.find((article) => {
+        return (
+          new Date(article.Data_x0020_Noticia || article.Created) <
+            new Date(this.item.Data_x0020_Noticia || this.item.Created) &&
+          article.GUID !== this.item.GUID
+        );
+      }) || { GUID: null };
+
+      let next = this.news.find((article) => {
+        return (
+          new Date(article.Data_x0020_Noticia || article.Created) >
+            new Date(this.item.Data_x0020_Noticia || this.item.Created) &&
+          article.GUID !== this.item.GUID
+        );
+      }) || { GUID: null };
+
+      return {
+        previous: prev,
+        next: next,
+      };
+    },
+  },
+  async mounted() {
+    window.mainExecution();
+
+    const data = await this.$http.get('tadpnnews.json');
+
+    this.news = data.data.d.results;
+
+    this.$route.meta.display = this.item.Title;
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/administrativo/tapm/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <q-header />
+    <q-bread-crumb />
+    <sub-menu />
+    <q-c-s-j-a-blog />
+    <q-c-s-strategic-plan :results="services" />
+    <hr />
+    <q-c-s-support-services :results="services" />
+    <hr />
+    <q-c-s-history :results="services" />
+    <hr />
+    <q-c-s-competencies :results="services" />
+    <q-footer />
+  </div>
+</template>
+
+<script>
+import QFooter from '@/components/Footer';
+import QHeader from '@/components/Header/Header';
+import QBreadCrumb from '@/components/BreadCrumb';
+import SubMenu from '@/views/aduaneiro/tadpn/components/SubMenu';
+import QCSSupportServices from '@/views/aduaneiro/tadpn/SupportService';
+import QCSStrategicPlan from '@/views/aduaneiro/tadpn/StrategicPlan';
+import QCSHistory from '@/views/aduaneiro/tadpn/History';
+import QCSCompetencies from '@/views/aduaneiro/tadpn/Competencies';
+import QCSJABlog from '@/components/TADPNBlog';
+
+export default {
+  name: 'QSectionOne',
+  components: {
+    QCSJABlog,
+    QCSCompetencies,
+    QCSHistory,
+    QCSStrategicPlan,
+    QCSSupportServices,
+    SubMenu,
+    QBreadCrumb,
+    QHeader,
+    QFooter,
+  },
+  data() {
+    return {
+      services: [],
+    };
+  },
+  mounted() {
+    window.mainExecution();
+
+    this.$http
+      .get('tadpninstituicao.json')
+      .then((data) => {
+        this.services = data.data.d.results;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style scoped></style>
