99f522d06a3798cdbe2c3ebeb534f1cd15bd62d9
Edson Chambal <echambal@ta.gov.mz>
2023-04-04 11:00:32 +0200

    Criacao dos Tribunais da Jurisdicao Administrativa
    Criacao dos Tribunais da Jurisdicao Administrativa
    Criacao da view do TA da Cidade de Maputo

---
+++ src/views/administrativo/tacm/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <q-header />
+    <q-bread-crumb />
+    <sub-menu />
+    <q-c-s-j-a-blog />
+    <q-c-s-strategic-plan :results="services" />
+    <hr />
+    <q-c-s-support-services :results="services" />
+    <hr />
+    <q-c-s-history :results="services" />
+    <hr />
+    <q-c-s-competencies :results="services" />
+    <q-footer />
+  </div>
+</template>
+
+<script>
+import QFooter from '@/components/Footer';
+import QHeader from '@/components/Header/Header';
+import QBreadCrumb from '@/components/BreadCrumb';
+import SubMenu from '@/views/aduaneiro/tadcm/components/SubMenu';
+import QCSSupportServices from '@/views/aduaneiro/tadcm/SupportService';
+import QCSStrategicPlan from '@/views/aduaneiro/tadcm/StrategicPlan';
+import QCSHistory from '@/views/aduaneiro/tadcm/History';
+import QCSCompetencies from '@/views/aduaneiro/tadcm/Competencies';
+import QCSJABlog from '@/components/TADCMBlog';
+
+export default {
+  name: 'QSectionOne',
+  components: {
+    QCSJABlog,
+    QCSCompetencies,
+    QCSHistory,
+    QCSStrategicPlan,
+    QCSSupportServices,
+    SubMenu,
+    QBreadCrumb,
+    QHeader,
+    QFooter,
+  },
+  data() {
+    return {
+      services: [],
+    };
+  },
+  mounted() {
+    window.mainExecution();
+
+    this.$http
+      .get('tadcminstituicao.json')
+      .then((data) => {
+        this.services = data.data.d.results;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style scoped></style>
