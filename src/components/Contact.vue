4d21e1370411ea0896966815069068ea8289539f
Edson Chambal <echambal@ta.gov.mz>
2023-04-14 13:18:53 +0200

    Update Contact.vue
    Rectificando a option adionada anteriormente

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -38,7 +38,7 @@
                 <div class="col-sm-6 col-md-6 col-lg-6">
                   <div class="form-group">
                     <select class="form-control" v-model="denuncia.Title">
-                      <option value="" disabled selected>Seleccione</option>
+                      <option value="">Seleccione</option>
                       <option v-for="(area, index) in areas" :key="index" :selected="area.Title == denuncia.Tipo"
                               :value="area.Title">
                         {{ area.Title }}
