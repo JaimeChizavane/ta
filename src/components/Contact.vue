a952cb965f53482a7a8e45b992de13b6fe07663e
Edson Chambal <echambal@ta.gov.mz>
2023-04-24 13:44:35 +0200

    Revert "Revert "Update Contact.vue""
    This reverts commit 424a82d30535223a4e9143e239c056ea25ef7e9c.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -38,9 +38,7 @@
                 <div class="col-sm-6 col-md-6 col-lg-6">
                   <div class="form-group">
                     <select class="form-control" v-model="denuncia.Title">
-                      <option :selected="true">Seleccione</option>
-                      <option v-for="(area, index) in areas" :key="index" 
-                      :selected="Seleccione"
+                      <option v-for="(area, index) in areas" :key="index" :selected="area.Title == denuncia.Tipo"
                               :value="area.Title">
                         {{ area.Title }}
                       </option>
