1683a067a1268eae7607d2a978c517a433ceeb1b
Candido Barato <50318137+Candido-Barato@users.noreply.github.com>
2024-02-05 09:37:00 +0200

    Add publicacoes do search

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import pt from 'vuetify/lib/locale/pt'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    locales: {pt },
+    current: 'pt',
 });
